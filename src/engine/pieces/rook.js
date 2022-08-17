519bbc4486c485e6e91294edc20ef9b372f92c2b
Oli Goodall <Oli.Goodall@softwire.com>
2022-08-17 16:05:56 +0100

    Rooks can't move anymore

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -12,14 +12,38 @@ export default class Rook extends Piece {
         let location = board.findPiece(this);
         let moves = [];
         
-        // horizontal movement
-        for (let i = 0; i <= 7; i++) {
+        // looking left
+        for (let i = 0; i <= location.col; i++) {
+            if (board.getPiece(Square.at(location.row, i))){
+                moves = []
+            }
             i === location.col ?
             moves = moves : moves.push(Square.at(location.row, i));
         }
 
-        // vertical movement
-        for (let i = 0; i <= 7; i++) {
+        // looking right
+        for (let i = location.col; i <= 7; i++) {
+            if (board.getPiece(Square.at(location.row, i))){
+                break
+            }
+            i === location.col ?
+            moves = moves : moves.push(Square.at(location.row, i));
+        }
+
+        // looking down
+        for (let i = 0; i <= location.row; i++) {
+            if (board.getPiece(Square.at(i, location.col))){
+                moves = []
+            }
+            i === location.row ?
+            moves = moves : moves.push(Square.at(i, location.col));
+        }
+
+        // looking up
+        for (let i = location.row; i <= 7; i++) {
+            if (board.getPiece(Square.at(i, location.col))){
+                break
+            }
             i === location.row ?
             moves = moves : moves.push(Square.at(i, location.col));
         }
