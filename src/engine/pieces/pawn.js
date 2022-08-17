667f5a265b222a6bfb9560518f4cbbfc0b396939
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-17 13:41:50 +0100

    Red-8 tests passed

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,23 +11,41 @@ export default class Pawn extends Piece {
         let location = board.findPiece(this)
         let moves = []
         if (this.player === Player.WHITE) {
-            if (board.getPiece(Square.at(location.row + 1, location.col))){
-                return moves = []
-            } if (location.row === 1){
-                // console.log(board.getPiece(Square.at(location.row, location.col))[1] + 'Hello Jack')
-                // moves.push(Square.at(location.row + 2, location.col))
+            if (!board.getPiece(Square.at(location.row + 1, location.col))) {
+                
+                if (location.row === 1){
+
+                    if (!board.getPiece((Square.at(location.row + 2, location.col)))) {
+
+                        moves.push(Square.at(location.row + 2, location.col))
+
+                    }
+                
+                }
+
+                moves.push(Square.at(location.row + 1, location.col))
+
+                }
+
+        } else {
+            
+            if (!board.getPiece(Square.at(location.row - 1, location.col))) {
+                
+                if (location.row === 6) {
+
+                    if (!board.getPiece((Square.at(location.row - 2, location.col)))) {
+
+                        moves.push(Square.at(location.row - 2, location.col))
+
+                    }
+
+                }
+
+                moves.push(Square.at(location.row - 1, location.col))
+                
+                }
             }
-            moves.push(Square.at(location.row + 1, location.col))
-            } else {
-            // if (){
-            //     return moves = []
-                if (location.row === 6){
-                moves.push(Square.at(location.row - 2, location.col))
-            }
-            moves.push(Square.at(location.row - 1, location.col))
-        } 
+
         return moves
     }
 }
-
-//Hello
