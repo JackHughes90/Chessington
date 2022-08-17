02406a981acea3edb97d9325ea14bb482594a6e0
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-17 16:34:34 +0100

    rook tests passed

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -13,39 +13,45 @@ export default class Rook extends Piece {
         let moves = [];
         
         // looking left
-        for (let i = 0; i <= location.col; i++) {
+        for (let i = 0; i < location.col; i++) {
             if (board.getPiece(Square.at(location.row, i))){
-                moves = []
+                moves = [];
+            } else {
+                moves.push(Square.at(location.row, i));
             }
-            i === location.col ?
-            moves = moves : moves.push(Square.at(location.row, i));
+            
         }
 
         // looking right
-        for (let i = location.col; i <= 7; i++) {
+        for (let i = 7; i > location.col; i--) {
             if (board.getPiece(Square.at(location.row, i))){
-                break
+                moves = [];
+            } else {
+                moves.push(Square.at(location.row, i));
             }
-            i === location.col ?
-            moves = moves : moves.push(Square.at(location.row, i));
+            
         }
 
         // looking down
-        for (let i = 0; i <= location.row; i++) {
+        for (let i = 0; i < location.row; i++) {
             if (board.getPiece(Square.at(i, location.col))){
-                moves = []
+                moves = [];
             }
-            i === location.row ?
-            moves = moves : moves.push(Square.at(i, location.col));
+            else {
+                moves.push(Square.at(i, location.col));
+            }
+            
         }
 
         // looking up
-        for (let i = location.row; i <= 7; i++) {
+        for (let i = 7; i > location.row; i--) {
             if (board.getPiece(Square.at(i, location.col))){
-                break
+                moves = [];
             }
-            i === location.row ?
-            moves = moves : moves.push(Square.at(i, location.col));
+            else {
+                moves.push(Square.at(i, location.col));
+            }
+            
         }
 
         return moves;
