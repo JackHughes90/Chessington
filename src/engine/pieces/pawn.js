ad02263bd57ae15a842217f4cc23f62b3096e17e
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-19 11:04:20 +0100

    pawn refactored

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -8,64 +8,62 @@ export default class Pawn extends Piece {
         super(player);
     }
 
+    isKing (board, square) {
+        return board.getPiece(square) instanceof King;
+    }
+
+
+
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
-        let moves = []
+        let location = board.findPiece(this);
+        let moves = [];
+        let locU1 =   Square.at(location.row + 1, location.col);
+        let locU2 =   Square.at(location.row + 2, location.col);
+        let locD1 =   Square.at(location.row - 1, location.col);
+        let locD2 =   Square.at(location.row - 2, location.col);
+        let locU1R1 = Square.at(location.row + 1, location.col + 1);
+        let locU1L1 = Square.at(location.row + 1, location.col - 1);
+        let locD1R1 = Square.at(location.row - 1, location.col + 1); 
+        let locD1L1 = Square.at(location.row - 1, location.col - 1);
+
         if (this.player === Player.WHITE && location.row != 7) {
-            if (!board.getPiece(Square.at(location.row + 1, location.col))) {
-                if (location.row === 1) {
-                    if (!board.getPiece((Square.at(location.row + 2, location.col)))) {
-                        moves.push(Square.at(location.row + 2, location.col))
-                    }
+            if (!board.getPiece(locU1)) {
+                if (location.row === 1 && !board.getPiece(locU2)) {
+                    moves.push(locU2);
                 }
-                moves.push(Square.at(location.row + 1, location.col))
+                moves.push(locU1);
             }
-            if (board.getPiece(Square.at(location.row + 1, location.col - 1))
-                && board.getPiece(Square.at(location.row + 1, location.col - 1)).player === Player.BLACK) {
-                    if (board.getPiece(Square.at(location.row + 1, location.col - 1)) instanceof King) {
-                        moves = moves;
-                    } else {
-                        moves.push(Square.at(location.row + 1, location.col - 1));
-                    }    
-                
+            if (board.getPiece(locU1L1) && board.getPiece(locU1L1).player === Player.BLACK) {
+                if (!this.isKing(board, locU1L1)) {
+                    moves.push(locU1L1);
+                }
             }
-            if (board.getPiece(Square.at(location.row + 1, location.col + 1))
-                && board.getPiece(Square.at(location.row + 1, location.col + 1)).player === Player.BLACK) {
-                    if (board.getPiece(Square.at(location.row + 1, location.col + 1)) instanceof King) {
-                        moves = moves;
-                    } else {
-                        moves.push(Square.at(location.row + 1, location.col + 1));
-                    } 
+            if (board.getPiece(locU1R1) && board.getPiece(locU1R1).player === Player.BLACK) {
+                if (!this.isKing(board, locU1R1)) {
+                    moves.push(locU1R1);
+                } 
             }
         }
         if (this.player === Player.BLACK && location.row != 0) { 
-            if (!board.getPiece(Square.at(location.row - 1, location.col))) { 
+            if (!board.getPiece(locD1)) { 
                 if (location.row === 6) {
-                    if (!board.getPiece((Square.at(location.row - 2, location.col)))) {
-                        moves.push(Square.at(location.row - 2, location.col))
+                    if (!board.getPiece((locD2))) {
+                        moves.push(locD2)
                     }
                 }
-                moves.push(Square.at(location.row - 1, location.col))
+                moves.push(locD1);
             }
-            if (board.getPiece(Square.at(location.row - 1, location.col - 1))
-                && board.getPiece(Square.at(location.row - 1, location.col - 1)).player === Player.WHITE) {
-
-                    if (board.getPiece(Square.at(location.row - 1, location.col - 1)) instanceof King) {
-                        moves = moves;
-                    } else {
-                        moves.push(Square.at(location.row - 1, location.col - 1));
+            if (board.getPiece(locD1L1)
+                && board.getPiece(locD1L1).player === Player.WHITE) {
+                    if (!this.isKing(board, locD1L1)) {
+                        moves.push(locD1L1);
                     }
-                
             }
-            if (board.getPiece(Square.at(location.row - 1, location.col + 1))
-                && board.getPiece(Square.at(location.row - 1, location.col + 1)).player === Player.WHITE) {
-
-                if (board.getPiece(Square.at(location.row - 1, location.col + 1)) instanceof King) {
-                    moves = moves;
-                } else {
-                    moves.push(Square.at(location.row - 1, location.col + 1));
+            if (board.getPiece(locD1R1)
+                && board.getPiece(locD1R1).player === Player.WHITE) {
+                if (!this.isKing(board, locD1R1)) {
+                    moves.push(locD1R1);
                 }
-                
             }
         }
         return moves
