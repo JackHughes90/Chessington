3f05fa7f0df144d49d27d176f19e1fc94766a2d9
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-19 12:08:08 +0100

    Red-13 Tests Passed

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,13 +1,24 @@
 import Piece from './piece';
 import Square from '../square';
 import Player from '../player';
+import King from './king';
 
 
 export default class Rook extends Piece {
     constructor(player) {
         super(player);
     }
+    // function to check what current player is (BLACK OR WHITE)
+    // combine these functions!!
+    
+    checkOpposingPiece(board,square){
+        return (this.player===board.getPiece(square).player);
+    }
 
+    isKing (board, square) {
+        return board.getPiece(square) instanceof King;
+    }
+    
     getAvailableMoves(board) {
         let location = board.findPiece(this);
         let moves = [];
@@ -16,6 +27,9 @@ export default class Rook extends Piece {
         for (let i = 0; i < location.col; i++) {
             if (board.getPiece(Square.at(location.row, i))){
                 moves = [];
+                if (!this.checkOpposingPiece(board, Square.at(location.row, i)) && !this.isKing(board, Square.at(location.row, i))) {
+                    moves.push(Square.at(location.row, i));
+                }
             } else {
                 moves.push(Square.at(location.row, i));
             }
@@ -26,6 +40,9 @@ export default class Rook extends Piece {
         for (let i = 7; i > location.col; i--) {
             if (board.getPiece(Square.at(location.row, i))){
                 moves = [];
+                if (!this.checkOpposingPiece(board, Square.at(location.row, i)) && !this.isKing(board, Square.at(location.row, i))) {
+                    moves.push(Square.at(location.row, i));
+                }
             } else {
                 moves.push(Square.at(location.row, i));
             }
@@ -36,6 +53,9 @@ export default class Rook extends Piece {
         for (let i = 0; i < location.row; i++) {
             if (board.getPiece(Square.at(i, location.col))){
                 moves = [];
+                if (!this.checkOpposingPiece(board, Square.at(i, location.col)) && !this.isKing(board, Square.at(i, location.col))) {
+                    moves.push(Square.at(i, location.col));
+                }
             }
             else {
                 moves.push(Square.at(i, location.col));
@@ -47,6 +67,9 @@ export default class Rook extends Piece {
         for (let i = 7; i > location.row; i--) {
             if (board.getPiece(Square.at(i, location.col))){
                 moves = [];
+                if (!this.checkOpposingPiece(board, Square.at(i, location.col)) && !this.isKing(board, Square.at(i, location.col))) {
+                    moves.push(Square.at(i, location.col));
+                }
             }
             else {
                 moves.push(Square.at(i, location.col));
